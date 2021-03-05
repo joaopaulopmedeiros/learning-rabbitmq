05f2b67afe793efacfc56aff7ac322aacf1f24c2
João Paulo <jpaulope@indracompany.com>
2021-03-05 12:40:27 -0300

    feat: demo configuration for connecting to sql server local insstance

--- src/dotnet/Banking.Api/Startup.cs
+++ src/dotnet/Banking.Api/Startup.cs
@@ -1,7 +1,9 @@
+using Banking.Infra.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace Banking.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<BankingContext>(
+                options => options.UseSqlServer(Configuration["ConnectionStrings:Database"])
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
