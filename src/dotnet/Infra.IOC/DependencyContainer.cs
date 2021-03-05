a8ec27c566e0cf70011658d6ef9451c3865b026d
João Paulo <jpaulope@indracompany.com>
2021-03-05 11:21:12 -0300

    feat: inversion of control

---
+++ src/dotnet/Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core.Bus;
+using Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
