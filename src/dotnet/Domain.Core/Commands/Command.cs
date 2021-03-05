1eef3d0b594bd06e96b6d169afeb689d5c5b7f1f
João Paulo <jpaulope@indracompany.com>
2021-03-05 10:23:18 -0300

    feat: interfaces and abstract classes for domain.core

---
+++ src/dotnet/Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Core.Events;
+
+namespace Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
